a138639389ce77f8be4613d5b1d8b372cdb50ef0
Sairam MJ <sairam@nanokernel.net>
2023-12-06 16:42:55 +0530

    Initial commit

---
+++ colossus/apps/notifications/context_processors.py
@@ -0,0 +1,6 @@
+def notifications(request):
+    if request.user.is_authenticated:
+        count = request.user.notifications.filter(is_seen=False).count()
+        return {"notifications_count": count}
+    else:
+        return dict()
